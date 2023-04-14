47049d0e96523c550e5524a0eee60ad9a39612be
gundalokesh <gundalokesh2000@gmail.com>
2023-04-14 18:26:31 +0530

    "add videos"

--- script.js
+++ script.js
@@ -1,11 +1,97 @@
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
 
-function openNav() {
-  // document.getElementById("mySidenav").style.width = "0";
-  // document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+let videos =
+[
+  {
+    "img":"https://i.ytimg.com/vi/yeNVBv41Vas/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNPKF",
+    "logo":"https://yt3.ggpht.com/ytc/AL5GRJXq93Lam_Y_AnANJuDorNxcSsTRWcd8_RMktuT09g=s68-c-k-c0x00ffffff-no-rj",
+    "discription":"Tech News #1208 : ChatGPT Bugs, Twitter For Sale, POCO F5 & F5Pro, Android 14, Bitcoin",
+    "channelname":"Telugu Tech Tutes",
+    "views":"1.5k views . 4  month ago ",
+
+  },
+  {
+    "img":"https://i.ytimg.com/vi/_UoLDxUH9BU/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBLPLm_36srP3ikHIyXD6ASyLvpWA",
+    "logo":"https://yt3.ggpht.com/ytc/AL5GRJX9P2pcrwng2Yu3EI0joMY-Wl0O2QqcxHlD8oCuvQ=s68-c-k-c0x00ffffff-no-rj",
+    "discription":"OnePlus Nord CE3 Lite 5G & OnePlus Nord Buds 2 Unboxing & initial impressions",
+    "channelname":"PrasadTechintelugu",
+    "views":"1k views . 1 month ago ",
+  },
+  {
+    "img":"https://i.ytimg.com/vi/Urwzk6ILvPQ/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBxoeO71QIX5ImYKGHhxN_Z-5jaOA",
+    "logo":"	https://yt3.ggpht.com/ytc/AGIKgqMl9TL4OpwS8Zc5jHrNnfO01f_Wbovye9mQzfp36Q=s88-c-k-c0x00ffffff-no-rj",
+    "discription":"Array Iteration: 8 Methods - map, filter, reduce, some, every, find, findIndex, forEach",
+    "channelname":"free code camp.org",
+    "views":"5k views . 12  days ",
+  },
+  {
+    "img":"https://i.ytimg.com/vi/R4MMtTLUZp4/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBYrTn1kfcka-WxSbdQQ1RrD-UojQ",
+    "logo":"https://yt3.ggpht.com/ycv9KBBnnHRRD8TWOmw0YaVuh9jNIdWYDT0HPiczC20AId79ewXOmmCq2V_Db4Vc4CQoEE6mgw=s88-c-k-c0x00ffffff-no-rj",
+    "discription":"JavaScript full course | JavaScript for beginners |JavaScript  for beginners  in Telugu |JavaScript",
+    "channelname":"Cs World Telugu",
+    "views":"5.5k views . 10  month ago ",
+  },
+  {
+    "img":"https://i.ytimg.com/vi/WpUxyY_HrdI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCZVhtH58_gvkcQBkQJTfN5ijj_7A",
+    "logo":"https://yt3.ggpht.com/ytc/AGIKgqOhsrolNvJopsFWtIjOJB2qIBFDTubuArFIfmR4=s88-c-k-c0x00ffffff-no-rj",
+    "discription":"Black Hole Explained | Unknown Facts about Space | In Telugu | Vikram Aditya Latest Videos | ",
+    "channelname":"Vikram Aditya",
+    "views":"446k views . 1 year ago ",
+  },
+  {
+    "img":"https://i.ytimg.com/vi/_UoLDxUH9BU/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBLPLm_36srP3ikHIyXD6ASyLvpWA",
+    "logo":"https://yt3.ggpht.com/ytc/AL5GRJX9P2pcrwng2Yu3EI0joMY-Wl0O2QqcxHlD8oCuvQ=s68-c-k-c0x00ffffff-no-rj",
+    "discription":"OnePlus Nord CE3 Lite 5G & OnePlus Nord Buds 2 Unboxing & initial impressions",
+    "channelname":"Telugu Tech Tutes",
+    "views":"1.5k views . 4  month ago ",
+  },
+  {
+    "img":"https://i.ytimg.com/vi/Urwzk6ILvPQ/hqdefault.jpg?s…AFwAcABBg==&rs=AOn4CLBxoeO71QIX5ImYKGHhxN_Z-5jaOA",
+    "logo":"	https://yt3.ggpht.com/ytc/AGIKgqMl9TL4OpwS8Zc5jHrNnfO01f_Wbovye9mQzfp36Q=s88-c-k-c0x00ffffff-no-rj",
+    "discription":"Tech News #1208 : ChatGPT Bugs, Twitter For Sale, POCO F5 & F5Pro, Android 14, Bitcoin",
+    "channelname":"Telugu Tech Tutes",
+    "views":"1.5k views . 4  month ago ",
+  },
+  {
+    "img":"	https://i.ytimg.com/vi/KgpFBdapobY/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAe56x2KyIipVWsLIG-M2NPBsR7aw",
+    "logo":"	https://yt3.ggpht.com/v_PwNTRdcmpaEU6zh9wytm0ERtq2BOAmBQvr1QyZstphlpcPUqjbX3wqIvSRR9bWIgSjmRUJcwE=s68-c-k-c0x00ffffff-no-rj",
+    "discription":"Show Me The Thumka (Song) Tu Jhoothi Main Makkaar |Ranbir,Shraddha",
+    "channelname":"T - Series",
+    "views":"5.5M views . 1 Week ",
+  },
+  {
+    "img":"https://i.ytimg.com/vi/V2W1g7iyNPQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCt5hOso2MNwNJxFoWIEyne3yFnhw",
+    "logo":"https://yt3.ggpht.com/ytc/AL5GRJVhr74d6xCzxKAVjE5axDa-3xQT5yZfvHYZAmEh=s68-c-k-c0x00ffffff-no-rj",
+    "discription":"Top 10 Most Important Global Issues of Today | The World Issues That Need Attention in 2021 by District10TV 2 years ago",
+    "channelname":"District 10Tv",
+    "views":"1.5k views . 4  month ago ",
+  },
+
+
+]
+ 
+const video =document.querySelector(".videos");
+ videos.forEach((item,i)=>{
+      const part = document.createElement("div");
+      part.className="part1";
+      part.innerHTML =`
+      <img class="imgvideo " src="${item.img}">
+      <div class="down d-flex">
+        <img class="channellogo mt-3" src="${item.logo}">
+       
+        
+        <div class="discription ms-4">
+          <p>${item.discription}</p>
+          <h6>${item.channelname}</h6>
+          <h6>${item.views}</h6>
+        </div>
+      
+      </div>
+      
+      
+      ` ;
+      video.appendChild(part);
+ });
+
+
+
+ 
